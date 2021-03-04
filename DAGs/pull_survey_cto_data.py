0a38fcd6825b0f5195949785088442b466df899a
Anas <anastiour@gmail.com>
2021-03-04 18:18:42 +0000

    feat(SCTO): create table of the form even if it has 0 submissions

--- DAGs/pull_survey_cto_data.py
+++ DAGs/pull_survey_cto_data.py
@@ -229,7 +229,8 @@ def save_data_to_db(**kwargs):
             for item in api_data
         ]
 
-        if isinstance(response_data, (list, )) and len(response_data):
+
+        if isinstance(response_data, (list, )):
             if SURV_DBMS is not None and (SURV_DBMS.lower() == 'postgres'
                                           or SURV_DBMS.lower().replace(
                                               ' ', '') == 'postgresdb'):
