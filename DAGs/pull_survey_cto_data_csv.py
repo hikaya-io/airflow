3c96fb94bf033cdf283f11a808ea55469166d53a
Anas <anastiour@gmail.com>
2021-03-23 01:23:23 +0000

    chore(SCTO): lint new DAG using Black linter

--- DAGs/pull_survey_cto_data_csv.py
+++ DAGs/pull_survey_cto_data_csv.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 
 from airflow import DAG, AirflowException
 from airflow.operators.python_operator import PythonOperator
-from helpers.utils import (DataCleaningUtil, logger)
+from helpers.utils import DataCleaningUtil, logger
 from helpers.mongo_utils import MongoOperations
 from helpers.postgres_utils import PostgresOperations
 from helpers.requests import create_requests_session
@@ -22,22 +22,24 @@ from helpers.configs import (
 
 logger = logging.getLogger(__name__)
 
-DAG_NAME = 'dots_survey_cto_data_csv_pipeline'
-PIPELINE = 'surveycto'
+DAG_NAME = "dots_survey_cto_data_csv_pipeline"
+PIPELINE = "surveycto"
 
 default_args = {
-    'owner': 'Hikaya-Dots',
-    'depends_on_past': False,
-    'start_date': get_daily_start_date(),
-    'catchup': False,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=5),
-    'on_failure_callback': notify(status='failed', pipeline=PIPELINE),
-    'on_success_callback': notify(status='success', pipeline=PIPELINE)
+    "owner": "Hikaya-Dots",
+    "depends_on_past": False,
+    "start_date": get_daily_start_date(),
+    "catchup": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "on_failure_callback": notify(status="failed", pipeline=PIPELINE),
+    "on_success_callback": notify(status="success", pipeline=PIPELINE),
 }
 
+
 def get_forms():
-    print('a')
+    print("a")
+
 
 def import_form_submissions(form):
     """
@@ -46,10 +48,7 @@ def import_form_submissions(form):
     # CSV direct
     csv_url = f"https://{SURV_SERVER_NAME}.surveycto.com/api/v1/forms/data/csv/{form.get('id')}"
     auth_basic = requests.auth.HTTPBasicAuth(SURV_USERNAME, SURV_PASSWORD)
-    test = requests.get(
-        csv_url,
-        auth=auth_basic
-    )
+    test = requests.get(csv_url, auth=auth_basic)
 
     # forms_request = session.get(
     #     f'https://{SURV_SERVER_NAME}.surveycto.com/console/forms-groups-datasets/get',
@@ -88,19 +87,18 @@ def import_form_submissions(form):
     print(df.info())
     print(df.describe())
 
-def import_forms_and_submissions(**kwargs):
-    import_form_submissions({
-        'id': 'airflow_sample_form'
-    })
 
-with DAG(DAG_NAME, default_args=default_args,
-         schedule_interval='@daily') as dag:
+def import_forms_and_submissions(**kwargs):
+    import_form_submissions({"id": "airflow_sample_form"})
+
+
+with DAG(DAG_NAME, default_args=default_args, schedule_interval="@daily") as dag:
 
     dag.doc_md = """
     Doc
     """
     t1 = PythonOperator(
-        task_id='Save_data_to_DB',
+        task_id="Save_data_to_DB",
         python_callable=import_forms_and_submissions,
         dag=dag,
     )
