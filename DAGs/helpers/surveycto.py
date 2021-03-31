5159424a7d4b85b8c44218f998825b0caecf7636
Anas <anastiour@gmail.com>
2021-03-31 20:25:52 +0100

    WIP(SCTO): helper class to encapsulate SurveyCTO requests

--- DAGs/helpers/surveycto.py
+++ DAGs/helpers/surveycto.py
@@ -1,14 +1,23 @@
+import requests
+
+from helpers.requests import create_requests_session
 
 
 class SurveyCTO:
-    def __init__(self, surver_name, username=None, password=None):
-        self.surver_name = surver_name
-        # self.client
-        # Fetch the CSRF toke
+    # ? When to raise error of wrong credentials
+    # TODO get logger
+    # TODO remove usaeg of Airflow exceptions
+    # TODO to what shape should data be transfered to? Are we using pandas here?
+    # TODO encrypted forms?
+    def __init__(self, server_name, username=None, password=None):
+        self.server_name = server_name
+        
+        # Get Requests session, build authentication and get CSRF token
+        self.session = create_requests_session(debug=False) # ? Can authentication and CSRF token be baked into the session?
+        self.auth_basic = requests.auth.HTTPBasicAuth(username, password)
         self.csrf_token = self._get_CSRF_token()
-        # self.session = 
 
-    def _get_CSRF_token():
+    def _get_CSRF_token(self):
         """Get a CSRF token from the SurveyCTO home page.
         Mainly used for logging in and generating a Python Requests session to make
         authenticated requests.
