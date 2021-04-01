3a257b5d0e69a8ad62027acce080682ac8189eb9
Anas <anastiour@gmail.com>
2021-04-01 02:18:47 +0100

    fix(postgres): disable debug in PostgresOperations

--- DAGs/helpers/postgres_utils_sqlalchemy.py
+++ DAGs/helpers/postgres_utils_sqlalchemy.py
@@ -1,3 +1,10 @@
+#######################################################################
+#         WORK IN PROGRESS                                            #
+#   * Explore usage of SQLAlchemy Core, althoug saving submissions is #
+#   now one using pandas.to_sql function.                             #
+#   * Explore using SQLAlchemy core to separate DBs by server         #
+#######################################################################
+
 import sqlalchemy
 from sqlalchemy import (
     create_engine,
@@ -14,38 +21,6 @@ class PostgresOperations:
     def __init__(self, user, password, host, port, database):
         self.engine = create_engine(
             f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}",
-            echo=True,
+            echo=False,
         )
 
-    def create_database(name):
-        print("create database")
-
-    def create_table(self, name, columns):
-        """Create a Postgres table
-
-        Args:
-            name (string): Name of the table
-            columns (array): array of dicts, each defining `name` of the field and its `type`
-                Allowed field types are the following: `string`, `integer`, `float`, `foreign_key?!` # TODO
-                # TODO Primary keys, nullability, foreign keys
-        """
-        try:
-            metadata = MetaData()
-            columns = []
-            table = Table(name, metadata, *columns)
-            metadata.create_all(self.engine)
-
-            return table
-        except e:
-            print('Exception')
-            print(e)
-
-    def upsert(table, value, on_conflict):
-        """Upsert a value into a table
-
-        Args:
-            table ([type]): [description]
-            value ([type]): [description]
-            on_conflict ([type]): key(s) on which to detect conflicting upserts
-        """
-        print("upsert")
