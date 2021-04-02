8bb06f7902ddbd0eeaab8589785117d5dc381b89
Anas <anastiour@gmail.com>
2021-04-02 01:06:34 +0100

    refactor(DAGs): Use Python "with" statement to manage DAG instance

--- DAGs/pull_comm_care_data.py
+++ DAGs/pull_comm_care_data.py
@@ -33,7 +33,6 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-dag = DAG('pull_data_from_comm_care', default_args=default_args)
 
 # set request headers and re-use
 request_headers = {"Authorization": "ApiKey {}:{}".format(
@@ -267,40 +266,48 @@ def clean_stale_data_on_db(**context):
 
             remove_deleted_submissions_from_db(instance_ids, form['name'])
 
+with DAG(
+    'pull_data_from_comm_care',
+    default_args=default_args
+) as dag:
+    dag.doc_md="""
+        ## CommCare export
+        > TODO document the DAG
+        #### Purpose
+        #### Outputs
+    """
+    get_comm_care_application_task = SimpleHttpOperator(
+        task_id='Get_Comm_Care_Application',
+        method='GET',
+        endpoint='/application/{}'.format(COMM_CARE_PROGRAM_ID),
+        http_conn_id='comm_care_base_url',
+        headers=request_headers,
+        xcom_push=True,
+        log_response=True,
+        dag=dag,
+    )
 
-# TASKS
-get_comm_care_application_task = SimpleHttpOperator(
-    task_id='Get_Comm_Care_Application',
-    method='GET',
-    endpoint='/application/{}'.format(COMM_CARE_PROGRAM_ID),
-    http_conn_id='comm_care_base_url',
-    headers=request_headers,
-    xcom_push=True,
-    log_response=True,
-    dag=dag,
-)
+    extract_forms_task = PythonOperator(
+        task_id="Extract_Forms",
+        python_callable=get_application_module_forms,
+        provide_context=True,
+        dag=dag,
+    )
 
-extract_forms_task = PythonOperator(
-    task_id="Extract_Forms",
-    python_callable=get_application_module_forms,
-    provide_context=True,
-    dag=dag,
-)
+    fetch_forms_data_task = PythonOperator(
+        task_id="Fetch_Forms_Data",
+        python_callable=get_forms_data,
+        provide_context=True,
+        dag=dag,
+    )
 
-fetch_forms_data_task = PythonOperator(
-    task_id="Fetch_Forms_Data",
-    python_callable=get_forms_data,
-    provide_context=True,
-    dag=dag,
-)
+    delete_stale_data_task = PythonOperator(
+        task_id='Delete_Stale_Data',
+        python_callable=clean_stale_data_on_db,
+        provide_context=True,
+        dag=dag,
+    )
 
-delete_stale_data_task = PythonOperator(
-    task_id='Delete_Stale_Data',
-    python_callable=clean_stale_data_on_db,
-    provide_context=True,
-    dag=dag,
-)
-
-# PIPELINES
-get_comm_care_application_task >> extract_forms_task >> fetch_forms_data_task
-extract_forms_task >> delete_stale_data_task
+    # PIPELINES
+    get_comm_care_application_task >> extract_forms_task >> fetch_forms_data_task
+    extract_forms_task >> delete_stale_data_task
